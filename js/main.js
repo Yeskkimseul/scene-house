09f3063b1806e518f22f4af72c56f145e8103c40
Yeskkimseul <yeslkim2@gmail.com>
2025-05-17 00:44:53 +0900

    mainpage update

--- js/main.js
+++ js/main.js
@@ -23,3 +23,18 @@ ScrollTrigger.scrollerProxy(scrollContainer, {
 locoScroll.on("scroll", ScrollTrigger.update);
 ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
 ScrollTrigger.refresh();
+
+setTimeout(() => {
+  locoScroll.update();      
+  ScrollTrigger.refresh();   
+}, 100);
+
+/* event 섹션션 */
+const eventItems = document.querySelectorAll('.sceneevent .eventlist li');
+
+eventItems.forEach(item => {
+  item.addEventListener('mouseenter', () => {
+    eventItems.forEach(el => el.classList.remove('active'));
+    item.classList.add('active');
+  });
+});
