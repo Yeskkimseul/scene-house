90f5e728573beb21c4c309081c6d9350b0bb218c
Yeskkimseul <yeslkim2@gmail.com>
2025-05-16 15:13:58 +0900

    update

---
+++ js/main.js
@@ -0,0 +1,25 @@
+const scrollContainer = document.querySelector("[data-scroll-container]");
+
+const locoScroll = new LocomotiveScroll({
+  el: scrollContainer,
+  smooth: true,
+  multiplier: 1.2
+});
+
+
+gsap.registerPlugin(ScrollTrigger);
+ScrollTrigger.scrollerProxy(scrollContainer, {
+  scrollTop(value) {
+    return arguments.length
+      ? locoScroll.scrollTo(value, 0, 0)
+      : locoScroll.scroll.instance.scroll.y;
+  },
+  getBoundingClientRect() {
+    return { top: 0, left: 0, width: window.innerWidth, height: window.innerHeight };
+  },
+  pinType: scrollContainer.style.transform ? "transform" : "fixed"
+});
+
+locoScroll.on("scroll", ScrollTrigger.update);
+ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+ScrollTrigger.refresh();
