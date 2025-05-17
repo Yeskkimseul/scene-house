741e38dc218731aeb3f45ff5d809d75b003caa68
Yeskkimseul <yeslkim2@gmail.com>
2025-05-18 01:16:33 +0900

    update

--- scene-house/js/contestdetail.js
+++
@@ -1,8 +0,0 @@
-  // 하트 아이콘 클릭 시
-    const hearts = document.querySelectorAll(".iconset li .heart-icon");
-    hearts.forEach(function (heart) {
-        heart.addEventListener("click", function () {
-            console.log("클릭");
-            this.classList.toggle("on");
-        });
-    });
