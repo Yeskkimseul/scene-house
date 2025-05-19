c59585597662d5fe45f85cffac1a038ca5f8957e
Yeskkimseul <yeslkim2@gmail.com>
2025-05-19 12:43:51 +0900

    update

---
+++ js/casting.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function () {
+    /* 모바일 gnb */
+    const closeBtn = document.querySelector('.top .close');
+    const ham = document.querySelector('.top .hamMenu');
+    const moblienav = document.querySelector('.mobilenav');
+
+    ham.addEventListener('click', function () {
+        this.classList.add('off');
+        closeBtn.classList.add('on');
+        moblienav.classList.add('on');
+        console.log('클릭')
+    });
+
+    closeBtn.addEventListener('click', function () {
+        this.classList.remove('on');
+        ham.classList.remove('off');
+        moblienav.classList.remove('on');
+    });
+});
