f7a93cd66c56afdb1a0e2ae61cbd8b070307247b
Yeskkimseul <yeslkim2@gmail.com>
2025-05-19 11:30:55 +0900

    moblie nav add

--- js/scenehouseplay.js
+++ js/scenehouseplay.js
@@ -1,9 +1,30 @@
 window.addEventListener('DOMContentLoaded', function () {
+
+  /* 모바일 gnb */
+  const closeBtn = document.querySelector('.top .close');
+  const ham = this.document.querySelector('.top .hamMenu');
+  const moblienav = this.document.querySelector('.mobilenav');
+  ham.addEventListener('click', function () {
+    this.classList.add('off');
+    closeBtn.classList.add('on')
+    moblienav.classList.add('on')
+  });
+
+  closeBtn.addEventListener('click',function(){
+    this.classList.remove('on');
+    ham.classList.remove('off');
+    moblienav.classList.remove('on');
+  })
+
+
+
+
+
   const wrapper = document.querySelector('.sceneswiper .swiper-wrapper');
   const slides = Array.from(wrapper.children);
   const speed = 0.4; /*speed */
 
-/* loop */
+  /* loop */
   slides.forEach(slide => {
     const clone = slide.cloneNode(true);
     wrapper.appendChild(clone);
@@ -13,7 +34,7 @@ window.addEventListener('DOMContentLoaded', function () {
 
   function animate() {
     x -= speed;
- /* reset */
+    /* reset */
     if (Math.abs(x) >= wrapper.scrollWidth / 2) {
       x = 0;
     }
